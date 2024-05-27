7e472c17572cd48ba36e94e802eb7d909dcba5e5
YAcodingroom <n12a18a@gmail.com>
2024-05-27 11:54:23 +0800

    add: add loading recipe feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,3 +11,20 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/664c8f193e7aa067e94e8706'
+    );
+    const data = await res.json();
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    const { recipe } = data.data;
+    console.log(recipe);
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+showRecipe();
