8a75d79800c099f68cd1addc02c0e36c8ceeb20b
YAcodingroom <n12a18a@gmail.com>
2024-05-27 13:28:07 +0800

    add: render recipe page

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,3 +1,5 @@
+import iconsPath from 'url:../img/icons.svg';
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -36,6 +38,105 @@ const showRecipe = async function () {
     const recipe = renameKeys(data.data.recipe);
 
     console.log(recipe);
+
+    const markup = `
+      <figure class="recipe__fig">
+        <img src="${recipe.imageUrl}" alt="${
+      recipe.title
+    }" class="recipe__img" />
+        <h1 class="${recipe.title}">
+          <span>Pasta with tomato cream sauce</span>
+        </h1>
+      </figure>
+
+      <div class="recipe__details">
+        <div class="recipe__info">
+          <svg class="recipe__info-icon">
+            <use href="${iconsPath}#icon-clock"></use>
+          </svg>
+          <span class="recipe__info-data recipe__info-data--minutes">${
+            recipe.cookingTime
+          }</span>
+          <span class="recipe__info-text">minutes</span>
+        </div>
+        <div class="recipe__info">
+          <svg class="recipe__info-icon">
+            <use href="${iconsPath}#icon-users"></use>
+          </svg>
+          <span class="recipe__info-data recipe__info-data--people">4</span>
+          <span class="recipe__info-text">${recipe.servings}</span>
+
+          <div class="recipe__info-buttons">
+            <button class="btn--tiny btn--increase-servings">
+              <svg>
+                <use href="${iconsPath}#icon-minus-circle"></use>
+              </svg>
+            </button>
+            <button class="btn--tiny btn--increase-servings">
+              <svg>
+                <use href="${iconsPath}#icon-plus-circle"></use>
+              </svg>
+            </button>
+          </div>
+        </div>
+
+        <div class="recipe__user-generated">
+          <svg>
+            <use href="${iconsPath}#icon-user"></use>
+          </svg>
+        </div>
+        <button class="btn--round">
+          <svg class="">
+            <use href="${iconsPath}#icon-bookmark-fill"></use>
+          </svg>
+        </button>
+      </div>
+
+      <div class="recipe__ingredients">
+        <h2 class="heading--2">Recipe ingredients</h2>
+        <ul class="recipe__ingredient-list">
+          ${recipe.ingredients
+            .map(ing => {
+              return `
+              <li class="recipe__ingredient">
+                <svg class="recipe__icon">
+                  <use href="${iconsPath}#icon-check"></use>
+                </svg>
+                <div class="recipe__quantity">${(ing.quantity ??= '')}</div>
+                <div class="recipe__description">
+                  <span class="recipe__unit">${ing.unit}</span>
+                  ${ing.description}
+                </div>
+              </li>
+            `;
+            })
+            .join('')}
+          
+        </ul>
+      </div>
+
+      <div class="recipe__directions">
+        <h2 class="heading--2">How to cook it</h2>
+        <p class="recipe__directions-text">
+          This recipe was carefully designed and tested by
+          <span class="recipe__publisher">${
+            recipe.publisher
+          }</span>.Please check out directions at their website.
+        </p>
+        <a
+          class="btn--small recipe__btn"
+          href="${recipe.sourceUrl}"
+          target="_blank"
+        >
+          <span>Directions</span>
+          <svg class="search__icon">
+            <use href="${iconsPath}#icon-arrow-right"></use>
+          </svg>
+        </a>
+      </div>
+    `;
+    recipeContainer.innerHTML = '';
+    recipeContainer.insertAdjacentHTML('afterbegin', markup);
   } catch (err) {
     console.error(err.message);
   }
